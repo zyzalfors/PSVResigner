f48ef1ec89f97d2dcb6263df3eb2d169a122f453
Al Armato <55386215+zyzalfors@users.noreply.github.com>
2026-09-10 23:33:38 +0200

    Add files via upload

--- PSVResigner.ps1
+++ PSVResigner.ps1
@@ -140,17 +140,6 @@ class PSVResigner {
 
     static [PSCustomObject] GetPSVInfo([string] $path) {
         $data = [IO.File]::ReadAllBytes($path)
-
-        if($data.Length -lt [PSVResigner]::TypeOffset + 1) {
-            return [PSCustomObject] @{
-                magic = "Unknown"
-                validMagic = $false
-                type = "Unknown"
-                sign = "Unknown"
-                validSign = $false
-            }
-        }
-
         $type = [int] $data[[PSVResigner]::TypeOffset]
         $sign = [PSVResigner]::GetSignature($data, $type)
 
